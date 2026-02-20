b95a670766ef7383811e265c7d37cdf2fdc999b7
OlegBeekeeper <a.bolotnyi@infrajump.com>
2026-02-20 18:28:18 +0200

    Add PowerShell postbuild script for Windows

---
+++ webapp/platform/mattermost-redux/postbuild.ps1
@@ -0,0 +1,3 @@
+Copy-Item -Path "../../channels/src/packages/mattermost-redux/src/selectors/create_selector/index.d.ts" -Destination "lib/selectors/create_selector/index.d.ts" -Force
+Copy-Item -Path "../../channels/src/packages/mattermost-redux/src/types/extend_redux.d.ts" -Destination "lib/types/extend_redux.d.ts" -Force
+Copy-Item -Path "../../channels/src/packages/mattermost-redux/src/types/extend_react_redux.d.ts" -Destination "lib/types/extend_react_redux.d.ts" -Force
