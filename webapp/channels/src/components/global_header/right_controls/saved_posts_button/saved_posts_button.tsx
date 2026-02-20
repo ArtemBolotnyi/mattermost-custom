5f5fa1ddce5541c741422686b7cdf7d65bf50f34
OlegBeekeeper <a.bolotnyi@infrajump.com>
2026-02-20 18:15:12 +0200

    Add counter to Saved messages button

--- webapp/channels/src/components/global_header/right_controls/saved_posts_button/saved_posts_button.tsx
+++ webapp/channels/src/components/global_header/right_controls/saved_posts_button/saved_posts_button.tsx
@@ -1,9 +1,13 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {FormattedMessage, useIntl} from 'react-intl';
 import {useDispatch, useSelector} from 'react-redux';
+import styled from 'styled-components';
+
+import {countFlaggedPosts} from 'mattermost-redux/actions/search';
+import {getFlaggedPostsCount} from 'mattermost-redux/selectors/entities/posts';
 
 import {closeRightHandSide, showFlaggedPosts} from 'actions/views/rhs';
 import {getRhsState} from 'selectors/rhs';
@@ -15,10 +19,38 @@ import {RHSStates} from 'utils/constants';
 
 import type {GlobalState} from 'types/store';
 
+const SavedPostsButtonContainer = styled.div`
+    position: relative;
+
+    .savedPostButton--count {
+        display: flex;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 16px;
+        height: 16px;
+        align-items: center;
+        justify-content: center;
+        background-color: var(--button-bg);
+        color: var(--button-color);
+        border-radius: 50%;
+        font-size: 9px;
+        pointer-events: none;
+    }
+`;
+
 const SavedPostsButton = (): JSX.Element | null => {
     const {formatMessage} = useIntl();
     const dispatch = useDispatch();
     const rhsState = useSelector((state: GlobalState) => getRhsState(state));
+    const flaggedCount = useSelector((state: GlobalState) => getFlaggedPostsCount(state));
+
+    useEffect(() => {
+        if (!flaggedCount) {
+            dispatch(countFlaggedPosts());
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const savedPostsButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
@@ -38,6 +70,7 @@ const SavedPostsButton = (): JSX.Element | null => {
                 />
             }
         >
+            <SavedPostsButtonContainer>
             <IconButton
                 icon={'bookmark-outline'}
                 toggled={rhsState === RHSStates.FLAG}
@@ -46,6 +79,12 @@ const SavedPostsButton = (): JSX.Element | null => {
                 aria-controls='searchContainer' // Must be changed if the ID of the container changes
                 aria-label={formatMessage({id: 'channel_header.flagged', defaultMessage: 'Saved messages'})}
             />
+                {
+                    flaggedCount > 0 && (
+                        <span className='savedPostButton--count'>{flaggedCount}</span>
+                    )
+                }
+            </SavedPostsButtonContainer>
         </WithTooltip>
     );
 };
